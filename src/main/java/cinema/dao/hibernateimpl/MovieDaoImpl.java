16ba3ffae73d928df8475abb33dae9cc4bde84c5
levko <levkosvd@gmail.com>
2020-02-04 07:54:10 +0200

    start project

---
+++ src/main/java/cinema/dao/hibernateimpl/MovieDaoImpl.java
@@ -0,0 +1,45 @@
+package cinema.dao.hibernateimpl;
+
+import cinema.dao.MovieDao;
+import cinema.lib.Dao;
+import cinema.model.Movie;
+import cinema.util.HibernateUtil;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private static final Logger LOGGER = Logger.getLogger(MovieDaoImpl.class);
+
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long movieId = (Long) session.save(movie);
+            transaction.commit();
+            movie.setId(movieId);
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert Movie entity", e);
+        }
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Movie.class);
+            criteriaQuery.from(Movie.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't insert Movie entity", e);
+        }
+    }
+}
