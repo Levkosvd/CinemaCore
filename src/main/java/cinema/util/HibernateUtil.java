dab91042fc08f176fa86f2f613b140145edf303a
levko <levkosvd@gmail.com>
2020-02-04 11:51:44 +0200

    checkstyle problem

--- src/main/java/cinema/util/HibernateUtil.java
+++ src/main/java/cinema/util/HibernateUtil.java
@@ -7,8 +7,7 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static SessionFactory sessionFactory  = initSessionFactory();
 
-    private HibernateUtil(){
-
+    private HibernateUtil() {
     }
 
     public static SessionFactory getSessionFactory() {
